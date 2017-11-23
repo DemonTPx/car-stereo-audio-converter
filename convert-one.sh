15f944b59c154f60cddf4f1aa8e3a36fb68eb426
Bert Hekman <demontpx@gmail.com>
2017-11-23 21:15:58 +0100

    Support different suffices; Now compatible with newer eyeD3

--- convert-one.sh
+++ convert-one.sh
@@ -4,11 +4,14 @@ set -e
 
 arg=$1
 f=${arg#in/}
+ext=${f##*.}
+fn=${f%.*}
+fnout=$fn.mp3
 
 d=`dirname "$f"`
 mkdir -p "out/$d"
 
-avconv -y -i "in/$f" -codec:a libmp3lame -b:a 192k -id3v2_version 3 -write_id3v1 1 -vn "out/$f" -v -8
-eyeD3 --remove-comments --remove-lyrics --remove-images "out/$f"
-eyeD3 --to-v1.1 "out/$f"
-eyeD3 --remove-v2 "out/$f"
+avconv -y -i "in/$f" -codec:a libmp3lame -b:a 192k -id3v2_version 3 -write_id3v1 1 -vn "out/$fnout" -v -8
+eyeD3 --remove-all-comments --remove-all-lyrics --remove-all-images "out/$fnout"
+eyeD3 --to-v1.1 "out/$fnout"
+eyeD3 --remove-v2 "out/$fnout"
